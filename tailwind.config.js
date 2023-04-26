fbaa3bd34208d8b7c956d821ded87eb0f672b4ad
toai.do <do.nguyentoai@tap-ic.co.jp>
2023-04-26 22:13:36 +0700

    create header with taiwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        25: "6.25rem",
+      },
+      colors: {
+        gray21: "rgb(21,32,43)",
+        upgrade: "#9763f6",
+        upgradehover: "#7d53cc",
+        certyfield: "#FFC0C7",
+        certyfieldhover: "#ffb3bb",
+        creatwebsite: "#FFF4A3",
+        cretehover: "#fff080",
+        signup: "#04AA6D",
+        signuphover: "#059862",
+        login: "#D9EEE1",
+        loginhover: "#059862",
+      },
+      fontFamily: { body: ["Source Sans Pro", "sans - serif"] },
+    },
   },
   plugins: [],
 };
